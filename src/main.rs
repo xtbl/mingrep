5a29a4242f95d53721cf7abc5948336ce48dfad1
Cristobal <correodecristobal@gmail.com>
2021-08-09 21:10:51 -0600

    added working params added modularity with lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::env;
+use std::process;
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
+
+    print_type_of(&config);
+
+    fn print_type_of<T>(_: &T) {
+        println!("type: {}", std::any::type_name::<T>())
+    }
 
     let first_item = args.get(1);
     match first_item {
@@ -12,5 +23,11 @@ fn main() {
         None => println!("none")
     }
 
-    println!("query {}, filename {}", query, filename)
+    println!("query: {}, filename: {}", config.query, config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    }
+
 }
