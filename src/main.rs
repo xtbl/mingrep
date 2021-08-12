7bc57b8429337d08bd3b9d91a0a077f2590a7b87
Cristobal <correodecristobal@gmail.com>
2021-08-11 20:36:18 -0600

    rust minigrep working

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,22 @@
 use std::env;
 use std::process;
 use minigrep::Config;
-use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {}", err);
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
 
     print_type_of(&config);
 
     fn print_type_of<T>(_: &T) {
-        println!("type: {}", std::any::type_name::<T>())
+        eprintln!("type: {}", std::any::type_name::<T>())
     }
 
-    let first_item = args.get(1);
-    match first_item {
-        Some(x) => println!("from Some first item {}", x),
-        None => println!("none")
-    }
-
-    println!("query: {}, filename: {}", config.query, config.filename);
-
-    if let Err(e) = run(config) {
-        println!("Application error {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error {}", e);
         process::exit(1);
     }
 
